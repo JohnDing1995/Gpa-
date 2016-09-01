b601262bf869084e881b450ed5f722b0a210e69f
JohnDing1995 <dry950526@gmail.com>
2016-09-01 21:38:09 +0800

    uploaded

---
+++ GPA_calculator.py
@@ -0,0 +1,93 @@
+import xlrd
+class Gpa_Calc:
+    def __init__(self,name,pku_gpa=None,zju_gpa=None,std_gpa=None,stdrev_gpa=None):
+        self.name = name
+        self.__pku_gpa = pku_gpa
+        self.__zju_gpa = zju_gpa
+        self.__std_gpa = std_gpa
+        self.__stdrev_gpa = stdrev_gpa
+    def pku(self, sh):
+        nrows = sh.nrows
+        grade_point = []
+        weight_list = []
+        for i in range(3, nrows-1):
+            grade = float(sh.cell(i,4).value)
+            weight = float(sh.cell(i,9).value)
+            grade_pku = 4 - 3 * (100 - grade) * (100 - grade) / 1600
+            grade_point.append(grade_pku*weight)
+            weight_list.append(weight)
+        gpa = sum(grade_point)/sum(weight_list)
+        self.__pku_gpa = gpa
+    def zju(self, sh):
+        nrows = sh.nrows
+        grade_point = []
+        weight_list = []
+        for i in range(3, nrows-1):
+            grade = float(sh.cell(i,4).value)
+            weight = float(sh.cell(i,9).value)
+            if grade<60:
+                grade_zju = 0
+            elif grade>=60 and grade<=85:
+                grade_zju = 4-(85-grade)/10
+            elif grade>85:
+                grade_zju = 4
+            grade_point.append(grade_zju*weight)
+            weight_list.append(weight)
+        gpa = sum(grade_point) / sum(weight_list)
+        self.__zju_gpa = gpa
+    def std(self, sh):
+        nrows = sh.nrows
+        grade_point = []
+        weight_list = []
+        for i in range(3, nrows-1):
+            grade = float(sh.cell(i,4).value)
+            weight = float(sh.cell(i,9).value)
+            if grade<60:
+                grade_std = 0
+            elif grade>=60 and grade<70:
+                grade_std = 1
+            elif grade>=70 and grade<80:
+                grade_std = 2
+            elif grade>=80 and grade<90:
+                grade_std = 3
+            else:
+                grade_std = 4
+            grade_point.append(grade_std*weight)
+            weight_list.append(weight)
+        gpa = sum(grade_point) / sum(weight_list)
+        self.__std_gpa = gpa
+    def stdrev(self, sh):
+        nrows = sh.nrows
+        grade_point = []
+        weight_list = []
+        for i in range(3, nrows-1):
+            grade = float(sh.cell(i,4).value)
+            weight = float(sh.cell(i,9).value)
+            if grade<60:
+                grade_stdrev = 0
+            elif grade>=60 and grade<75:
+                grade_stdrev = 2
+            elif grade>=75 and grade<85:
+                grade_stdrev = 3
+            else:
+                grade_stdrev = 4
+            grade_point.append(grade_stdrev*weight)
+            weight_list.append(weight)
+        gpa = sum(grade_point) / sum(weight_list)
+        self.__stdrev_gpa = gpa
+    def get_pkugpa(self):
+        return self.__pku_gpa
+    def get_zjugpa(self):
+        return self.__zju_gpa
+    def get_stdgpa(self):
+        return self.__std_gpa
+    def get_stdrevgpa(self):
+        return self.__stdrev_gpa
+def get_sheet(name):
+    fname = name
+    bk = xlrd.open_workbook(fname)
+    try:
+        sh = bk.sheet_by_index(0)
+    except:
+        print("no sheet in %s named Sheet1" % fname)
+    return sh
---
+++ gpa.py
@@ -0,0 +1,12 @@
+from GPA_calculator import Gpa_Calc, get_sheet
+file_name = input('请输入文件名，包含后缀')
+sheet = get_sheet(file_name)
+ruiyang = Gpa_Calc('ruiyang')
+ruiyang.pku(sheet)
+ruiyang.zju(sheet)
+ruiyang.std(sheet)
+ruiyang.stdrev(sheet)
+print("北大算法 %f"%ruiyang.get_pkugpa())
+print("浙大算法 %f"%ruiyang.get_zjugpa())
+print("标准算法 %f"%ruiyang.get_stdgpa())
+print("标准改进算法 %f"%ruiyang.get_stdrevgpa())
